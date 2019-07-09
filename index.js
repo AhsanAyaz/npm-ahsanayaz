c51656643c2d9e4207eee72bfa46c66e3e256153
Ahsan Ayaz <ahsan.ubitian@gmail.com>
2019-07-09 16:58:51 +0800

    release(1.0.3)

--- index.js
+++ index.js
@@ -11,16 +11,20 @@ const gde = `${blueBright('  Google Developers Expert')} ${white('in Angular')}`
 const twitter = blueBright('https://twitter.com/ahsan_ayz')
 const linkedIn = blueBright('https://linkedin.com/in/ahsanayaz')
 const github = blueBright('https://github.com/ahsanayaz')
+const slides = blueBright('https://slides.com/ahsanayaz')
+const medium = blueBright('https://medium.com/@ahsan.ayaz')
 const npx = white('npx ahsanayaz')
 
 const newline = '\n'
-const heading = `${white('  Ahsan Ayaz /')} ${blueBright('ahsanayaz')}`
+const heading = `${white('  Muhammad Ahsan Ayaz /')} ${blueBright('ahsanayaz')}`
 const working = `${white(bold('     Work:'))}  ${work}`
 const linkedining = `${white(bold('  LinkedIn:'))} ${linkedIn}`
 const twittering = `${white(bold('  Twitter:'))}  ${twitter}`
 const githubing = `${white(bold('   GitHub:'))}  ${github}`
+const slidesing = `${white(bold('   Slides:'))}  ${slides}`;
+const mediuming = `${white(bold('   Medium:'))}  ${medium}`;
 const carding = `${white(bold('     Card:'))}  ${npx}`
 
-let output = `${newline} ${newline} ${heading} ${newline} ${gde} ${newline} ${newline} ${working} ${newline} ${twittering} ${newline} ${linkedining} ${newline} ${githubing} ${newline}  ${newline} ${carding} ${newline} ${newline}`;
+let output = `${newline} ${newline} ${heading} ${newline} ${gde} ${newline} ${newline} ${working} ${newline} ${twittering} ${newline} ${linkedining} ${newline} ${githubing} ${newline} ${slidesing} ${newline} ${mediuming} ${newline} ${newline} ${carding} ${newline} ${newline}`;
 
 console.log(output)
